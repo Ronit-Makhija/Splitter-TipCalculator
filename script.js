e3e88b4e76b4b6127f3d4ad43190b19d9eaa146f
Ronit-Makhija <92474662+Ronit-Makhija@users.noreply.github.com>
2021-10-29 13:07:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ var array = [];
 var a = 0;
 
 
-$(".custom").click(function(){
-  array.pop($(".button-n").html());
-  $(".button-n").removeClass("button-click");
+$(".custom-tip").click(function(){
+  array.pop($(".tip-buttons").html());
+  $(".tip-buttons").removeClass("button-click");
   a = 1;
 })
 
-$(".button-n").click(function(){
-  array.pop($(".button-n").html());
-  $(".custom").val('');
-  $(".button-n").removeClass("button-click");
+$(".tip-buttons").click(function(){
+  array.pop($(".tip-buttons").html());
+  $(".custom-tip").val('');
+  $(".tip-buttons").removeClass("button-click");
   $(this).addClass("button-click");
   let x = $(this).html();
   let y = x.slice(0,x.length - 1);
@@ -21,47 +21,47 @@ $(".button-n").click(function(){
 
 
 
-$(".buttonr").click(function(){
-  $(".input1").val('');
-  $(".button-n").removeClass("button-click");
+$(".reset-button").click(function(){
+  $(".inputs").val('');
+  $(".tip-buttons").removeClass("button-click");
   $(".tip-ammount").html("$0.0");
   $(".total-bill").html("$0.0");
   $(".p1").html("");
   $(".p2").html("");
-  $(".input2").removeClass("error");
-  $(".bill1").removeClass("error");
+  $(".nop-input").removeClass("error");
+  $(".bill-input").removeClass("error");
 })
 
-$(".cal").click(function(){
-  if($(".input2").val() === "0" || $(".input2").val() === "" || $(".bill1").val() === "0" || $(".bill1").val() === "0"){
+$(".calculate-button").click(function(){
+  if($(".nop-input").val() === "0" || $(".nop-input").val() === "" || $(".bill-input").val() === "0" || $(".bill-input").val() === "0"){
 
-    if(($(".bill1").val() === "0" || $(".bill1").val() === "") && ($(".input2").val() !== "0" && $(".input2").val() !== "")){
+    if(($(".bill-input").val() === "0" || $(".bill-input").val() === "") && ($(".nop-input").val() !== "0" && $(".nop-input").val() !== "")){
       $(".p1").html("Can't be Zero!");
-      $(".bill1").addClass("error");
+      $(".bill-input").addClass("error");
     }
 
-    else if (($(".input2").val() === "0" || $(".input2").val() === "") &&  ($(".bill1").val() !== "0" && $(".bill1").val() !== "")){
+    else if (($(".nop-input").val() === "0" || $(".nop-input").val() === "") &&  ($(".bill-input").val() !== "0" && $(".bill-input").val() !== "")){
       $(".p2").html("Can't be Zero!");
-      $(".input2").addClass("error");
+      $(".nop-input").addClass("error");
     }
     else{
       $(".p1").html("Can't be Zero!");
-      $(".bill1").addClass("error");
+      $(".bill-input").addClass("error");
       $(".p2").html("Can't be Zero!");
-      $(".input2").addClass("error");
+      $(".nop-input").addClass("error");
     }
   }
 
   else{
-    $(".input2").removeClass("error");
-    $(".bill1").removeClass("error");
+    $(".nop-input").removeClass("error");
+    $(".bill-input").removeClass("error");
     $(".p1").html("");
     $(".p2").html("");
-    var bill = $(".bill1").val();
+    var bill = $(".bill-input").val();
 
 
     if(a === 1){
-      var tip = bill * ($(".custom").val()) / 100;
+      var tip = bill * ($(".custom-tip").val()) / 100;
       a = 0;
     }
     else{
@@ -69,7 +69,7 @@ $(".cal").click(function(){
     }
 
 
-    var numOfPerson = $(".input2").val();
+    var numOfPerson = $(".nop-input").val();
     var tipPerPerson = tip / numOfPerson;
     $(".tip-ammount").html("$" + Math.round(tipPerPerson));
 
